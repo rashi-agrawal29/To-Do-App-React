10f8661a280a6ebe31d79db53b10772e09afd40b
Rashi <rashiagrawal299@gmail.com>
2021-11-27 15:48:29 +0530

    build

---
+++ build/precache-manifest.d95f8ffe7441d81a54a90bd12ec77d7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd47536210770f6e130d9fd25a23fda5",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "3fd9080a82fe37a936e7",
+    "url": "/To-Do-App-in-React/static/css/main.02fe4419.chunk.css"
+  },
+  {
+    "revision": "27130051255a819fa7ea",
+    "url": "/To-Do-App-in-React/static/js/2.7bd08d42.chunk.js"
+  },
+  {
+    "revision": "3fd9080a82fe37a936e7",
+    "url": "/To-Do-App-in-React/static/js/main.aa1416f2.chunk.js"
+  },
+  {
+    "revision": "759cf7b4cf17cf63d488",
+    "url": "/To-Do-App-in-React/static/js/runtime~main.60884708.js"
+  }
+]);
